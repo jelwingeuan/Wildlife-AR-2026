6dc9aaffb0fa2e007866c671f43d44a71898dff0
MK2018-ai <mklau2758@gmail.com>
2026-05-23 13:01:39 +0800

    Added Tapir and Lar Gibbon
    Tapir works, Lar Gibbon no function

---
+++ WildlifeFriend-ElephantTestV2_BackUpThisFolder_ButDontShipItWithYourGame/il2cppOutput/Il2CppMetadataRegistration.c
@@ -0,0 +1,36 @@
+﻿#include "pch-c.h"
+
+
+#include "codegen/il2cpp-codegen-metadata.h"
+
+
+
+
+
+extern Il2CppGenericClass* const g_Il2CppGenericTypes[];
+extern const Il2CppGenericInst* const g_Il2CppGenericInstTable[];
+extern const Il2CppGenericMethodFunctionsDefinitions g_Il2CppGenericMethodFunctions[];
+extern const Il2CppType* const  g_Il2CppTypeTable[];
+extern const Il2CppMethodSpec g_Il2CppMethodSpecTable[];
+extern const int32_t* g_FieldOffsetTable[];
+extern const Il2CppTypeDefinitionSizes* g_Il2CppTypeDefinitionSizesTable[];
+IL2CPP_EXTERN_C const Il2CppMetadataRegistration g_MetadataRegistration;
+const Il2CppMetadataRegistration g_MetadataRegistration = 
+{
+	15482,
+	g_Il2CppGenericTypes,
+	11335,
+	g_Il2CppGenericInstTable,
+	140406,
+	g_Il2CppGenericMethodFunctions,
+	44475,
+	g_Il2CppTypeTable,
+	163914,
+	g_Il2CppMethodSpecTable,
+	12188,
+	g_FieldOffsetTable,
+	12188,
+	g_Il2CppTypeDefinitionSizesTable,
+	0,
+	NULL,
+};
